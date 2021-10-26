1fb2146c0d7867265cb7b9a39c1bd6c808e6c645
Wobi848 <wobi@4wobi.com>
2021-10-26 13:15:04 -0400

    master

--- Angular/learn-project/src/app/app.module.ts
+++ Angular/learn-project/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 
 import { CoursesService } from './course/courses.service';
+import { SummaryPipe } from './course/summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
--- Angular/learn-project/src/app/course/course.component.ts
+++ Angular/learn-project/src/app/course/course.component.ts
@@ -12,11 +12,36 @@ export class CourseComponent implements OnInit {
   courses: any;
   imageUrl = "https://4wobi.com/wp-content/uploads/2021/08/Wobi_Logo-croped.svg";
   colSpan = 2;
+  isActive = true;
+
+  email = "email@adress.com";
+
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.96444,
+    students: 123123,
+    price: 991.29,
+    releaseDate: new Date(2020, 2, 1)
+  }
+
+  costumPipe = `Lorem ipsum dolor sit,
+                amet consectetur adipisicing elit. Impedit sequi animi deserunt mollitia explicabo,
+                illum, pariatur officia qui, aliquam eos rerum iure
+                sed culpa enim officiis ea asperiores a veritatis `;
 
   constructor(private service: CoursesService) { }
 
   ngOnInit(): void {
     this.courses = this.service.getCourses();
+  }
+
+  onEvent($event: any) {
+    $event.stopPropagation();
+    console.log("Button clicked", $event);
+  }
+
+  onKeyUp() {
+    console.log(this.email);
   }
 
 }
